0dcd63db1c514bb08bae14975454eed2110fe796
pavel-genuine <pavel.palystore@gmail.com>
2022-02-17 00:32:29 +0600

    complete without fn

--- js/script.js
+++ js/script.js
@@ -16,27 +16,23 @@ document.getElementById('calc-btn').addEventListener('click', function () {
 
         document.getElementById('balance').innerText = parseFloat(income) - parseFloat(totalExpense)
     }
-
     else {
 
         document.getElementById('balance').innerText = 'expense is higher!'
     }
+})
 
+document.getElementById('save-btn').addEventListener('click', function () {
+
+    const income = document.getElementById('income').value
+    const savePercentage = document.getElementById('save-percentage').value
+    const savingAmount = document.getElementById('saving-amount').innerText = parseFloat(income) * parseFloat(savePercentage) / 100
+
+
+    const balance = document.getElementById('balance').innerText
+
+    document.getElementById('remain-balance').innerText = parseFloat(balance) - parseFloat(savingAmount)
 
 
 
 })
-
-
-
-
-// function expnse(food, rent, cloth) {
-
-//     const foodValue = document.getElementById('food').value = food
-//     const rentValue = document.getElementById('rent').value = rent
-//     const clothValue = document.getElementById('cloth').value = cloth
-
-//     document.getElementById('total-expense').innerText = parseFloat(foodValue) + parseFloat(rentValue) + parseFloat(clothValue)
-
-
-// }
