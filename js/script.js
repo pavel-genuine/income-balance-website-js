db019889a239e6e2c66e01601645ae52d64bc866
pavel-genuine <pavel.palystore@gmail.com>
2022-02-16 23:41:38 +0600

    js starting

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+const buttons = document.getElementsByClassName('btn')
+
+for (const button of buttons) {
+
+    button.addEventListener('click', function () {
+
+        if (button.innerText = 'Calculate') {
+
+
+
+        }
+
+        else if (button.innerText = 'Save') {
+
+        }
+
+    })
+
+}
+
+
+function expnse(food, rent, cloth) {
+
+}
