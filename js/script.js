cb0bdd4ba7273631326ed12d19cedbf1d48061f4
pavel-genuine <pavel.palystore@gmail.com>
2022-02-17 00:13:40 +0600

    first js commit

--- js/script.js
+++ js/script.js
@@ -1,25 +1,42 @@
 
-const buttons = document.getElementsByClassName('btn')
-
-for (const button of buttons) {
-
-    button.addEventListener('click', function () {
-
-        if (button.innerText = 'Calculate') {
 
 
-
-        }
-
-        else if (button.innerText = 'Save') {
-
-        }
-
-    })
-
-}
+document.getElementById('calc-btn').addEventListener('click', function () {
 
 
-function expnse(food, rent, cloth) {
+    const foodValue = document.getElementById('food').value
+    const rentValue = document.getElementById('rent').value
+    const clothValue = document.getElementById('cloth').value
 
-}
+    const totalExpense = document.getElementById('total-expense').innerText = parseFloat(foodValue) + parseFloat(rentValue) + parseFloat(clothValue)
+
+    const income = document.getElementById('income').value
+
+    if (income > totalExpense) {
+
+        document.getElementById('balance').innerText = parseFloat(income) - parseFloat(totalExpense)
+    }
+
+    else {
+
+        document.getElementById('balance').innerText = 'expense is higher!'
+    }
+
+
+
+
+})
+
+
+
+
+// function expnse(food, rent, cloth) {
+
+//     const foodValue = document.getElementById('food').value = food
+//     const rentValue = document.getElementById('rent').value = rent
+//     const clothValue = document.getElementById('cloth').value = cloth
+
+//     document.getElementById('total-expense').innerText = parseFloat(foodValue) + parseFloat(rentValue) + parseFloat(clothValue)
+
+
+// }
