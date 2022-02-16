29829157ac58f93c0f13d099c0633b1a9e5378c3
pavel-genuine <pavel.palystore@gmail.com>
2022-02-17 01:11:53 +0600

    error messege

--- js/script.js
+++ js/script.js
@@ -8,11 +8,24 @@ function balanceAmount(id, firstAmount, secondAmount) {
 document.getElementById('calc-btn').addEventListener('click', function () {
 
 
-    const foodValue = document.getElementById('food').value
-    const rentValue = document.getElementById('rent').value
-    const clothValue = document.getElementById('cloth').value
+    const foodValue = parseFloat(document.getElementById('food').value)
+    const rentValue = parseFloat(document.getElementById('rent').value)
+    const clothValue = parseFloat(document.getElementById('cloth').value)
 
-    const totalExpense = document.getElementById('total-expense').innerText = parseFloat(foodValue) + parseFloat(rentValue) + parseFloat(clothValue)
+    // if (typeof foodValue != 'number' && typeof rentValue != 'number' && typeof clothValue != 'number') {
+
+    //     document.getElementById('total-expense').innerText = 'please enter number!'
+
+
+    // }
+
+    // else {
+
+    const totalExpense = document.getElementById('total-expense').innerText = foodValue + rentValue + clothValue
+
+    // }
+
+
 
     const income = document.getElementById('income').value
 
@@ -31,15 +44,28 @@ document.getElementById('save-btn').addEventListener('click', function () {
 
     const income = document.getElementById('income').value
     const savePercentage = document.getElementById('save-percentage').value
-    const savingAmount = document.getElementById('saving-amount').innerText = parseFloat(income) * parseFloat(savePercentage) / 100
+
+    if (savePercentage >= 0) {
+        const savingAmount = document.getElementById('saving-amount').innerText = parseFloat(income) * parseFloat(savePercentage) / 100
+
+    }
+    // else {
+
+    // }
+
 
 
     const balance = document.getElementById('balance').innerText
 
     // document.getElementById('remain-balance').innerText = parseFloat(balance) - parseFloat(savingAmount)
 
-    balanceAmount('remain-balance', balance, savingAmount)
+    if (balance > savingAmount) {
+        balanceAmount('remain-balance', balance, savingAmount)
 
+    }
+    else {
 
+        document.getElementById('remain-balance').innerTex = 'Saving is higher than Balance!'
 
+    }
 })
