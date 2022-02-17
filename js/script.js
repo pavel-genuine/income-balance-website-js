7b14158ae7eda18ed40f60a39dbb68c4e8e3d1aa
pavel-genuine <pavel.palystore@gmail.com>
2022-02-17 07:51:14 +0600

    project completion

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 
+// common function for balanceAmount 
 function balanceAmount(id, firstAmount, secondAmount) {
 
     const balance = document.getElementById(id).innerText = parseFloat(firstAmount) - parseFloat(secondAmount)
@@ -6,13 +7,7 @@ function balanceAmount(id, firstAmount, secondAmount) {
 }
 
 
-document.getElementById('calc-btn').addEventListener('click', function () {
-
-    document.getElementById('total-expense').innerText = expense()
-    document.getElementById('balance').innerText = balance()
-
-})
-
+// function for expense calculation 
 function expense() {
 
     const foodValue = document.getElementById('food').value
@@ -24,57 +19,74 @@ function expense() {
         const totalExpense = document.getElementById('total-expense').innerText = parseFloat(foodValue) + parseFloat(rentValue) + parseFloat(clothValue)
 
         return totalExpense;
-
     }
 
     else {
 
-        const totalExpense = document.getElementById('total-expense').innerText = 'please enter valid input!'
+        const totalExpense = document.getElementById('total-expense').innerText = '❌ invalid input!'
         return totalExpense;
     }
 }
 
+// function for balance calculation
 function balance() {
 
     const income = document.getElementById('income').value
 
     const totalExpense = expense()
 
-    if (income > totalExpense) {
+    if (income > totalExpense && income > 0) {
 
+        // calling balanceAmount() function - 1st time
         const balance = balanceAmount('balance', income, totalExpense)
         return balance
     }
     else {
 
-        const balance = document.getElementById('balance').innerText = 'expense is higher!'
+        const balance = document.getElementById('balance').innerText = '❌ imbalanced!'
         return balance
     }
 }
 
+// calc-btn eventListener 
+document.getElementById('calc-btn').addEventListener('click', function () {
+
+    // calling expense() function
+    document.getElementById('total-expense').innerText = expense()
+
+    // calling balance() function - 1st time
+    document.getElementById('balance').innerText = balance()
+
+})
+
+// save-btn eventListener 
 document.getElementById('save-btn').addEventListener('click', function () {
 
     const income = document.getElementById('income').value
     const savePercentage = document.getElementById('save-percentage').value
 
-    const balanceValue = balance()
+    // calling balance() function -2nd time
+    const balanceValue = document.getElementById('balance').innerText = balance()
 
-    if (savePercentage >= 0) {
+    if (savePercentage >= 0 && savePercentage < 100) {
 
         const savingAmount = document.getElementById('saving-amount').innerText = parseFloat(income) * parseFloat(savePercentage) / 100
 
-        if (balanceValue >= savingAmount) {
+        if (savingAmount < balanceValue) {
 
+            // calling balanceAmount() function - 2nd time
             balanceAmount('remain-balance', balanceValue, savingAmount)
 
-        }
-        else {
+        } else {
 
-            document.getElementById('remain-balance').innerTex = 'Saving is higher than Balance!'
+            document.getElementById('remain-balance').innerText = '❌ inappropriate'
+
 
         }
+
     }
     else {
-        savingAmount.innerText = 'error'
+        document.getElementById('saving-amount').innerText = 'error❌!'
+        document.getElementById('remain-balance').innerText = '❌ inappropriate'
     }
 })
